fcaef0343f5ad19970ee221c9e1cedfe63b3d0b0
adrianht <adrianhthompson@gmail.com>
2019-10-24 11:33:51 +0200

    task1a

--- tasks/task1.scala
+++ tasks/task1.scala
@@ -1,3 +1,7 @@
-object Hello extends App {
-  println("Hello World")
+object Task1 extends App {
+  var myArray = Array.empty[Int]
+  for(i <- 1 to 50){
+    myArray = myArray :+ i
+  }
+  myArray.foreach{println}
 }
