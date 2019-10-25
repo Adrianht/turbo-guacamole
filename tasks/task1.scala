796b3e5d3702fd171e2088a0bae6d05b793eb2c0
adrianht <adrianhthompson@gmail.com>
2019-10-25 10:43:38 +0200

    task1b

--- tasks/task1.scala
+++ tasks/task1.scala
@@ -1,7 +1,21 @@
 object Task1 extends App {
-  var myArray = Array.empty[Int]
-  for(i <- 1 to 50){
-    myArray = myArray :+ i
+
+  // Task 1)a)
+  def Task1a {
+    var myArray : Array[Int] = Array.empty[Int]
+    for(i <- 1 to 50){
+      myArray = myArray :+ i
+    }
+    myArray.foreach{println}
   }
-  myArray.foreach{println}
+  println(Task1a)
+  
+  def Task1b(sumArray: Array[Int]): Int = {
+    var sum : Int = 0
+    for(value <- sumArray){
+      sum += value
+    }
+    return sum
+  }
+  println(Task1b(Array(1,2,3,4,5)))
 }
