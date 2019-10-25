15ea97d77e5c7f0811c5f02a43e860a762b457d7
adrianht <adrianhthompson@gmail.com>
2019-10-25 15:35:58 +0200

    update file locations and finish task 1-3

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -9,8 +9,16 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = balance.amount -= amount;
-    def deposit (amount: Double): Unit = balance.amount += amount;
+    def withdraw(amount: Double): Either[Unit, String] = this.synchronized { 
+      if(amount > initialBalance || 0 > amount) return Right("Cant withdraw more than what your balance is") 
+      Left(balance.amount -= amount)
+    }
+
+    def deposit (amount: Double): Either[Unit, String] = this.synchronized {
+      if(amount <= 0) return Right("Cant deposit negative amount")
+      Left(balance.amount += amount)
+    }
+
     def getBalanceAmount: Double       = balance.amount;
 
     def transferTo(account: Account, amount: Double) = {
